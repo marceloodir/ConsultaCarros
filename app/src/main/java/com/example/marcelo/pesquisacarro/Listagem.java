a8c5c1bcf71364fe5bce144f3a82c1fecad4b6c8
Marcelo Odir <marceloodir@gmail.com>
2015-08-10 21:53:53 -0300

    Primeira parte das views pronta.

---
+++ app/src/main/java/com/example/marcelo/pesquisacarro/Listagem.java
@@ -0,0 +1,37 @@
+package com.example.marcelo.pesquisacarro;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class Listagem extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listagem);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_listagem, menu);
+        return true;
+    }
+
+
+    public void logout(MenuItem item) {
+
+    }
+
+    public void adicionar(View view) {
+
+    }
+
+    public void consultar(View view) {
+
+    }
+}
---
+++ app/src/main/java/com/example/marcelo/pesquisacarro/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.marcelo.pesquisacarro;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+
+    public void logar(View view) {
+        Intent intent = new Intent(this, Listagem.class);
+        startActivity(intent);
+    }
+
+    public void cadastrar(MenuItem item) {
+
+    }
+}
